22d69e48f513288563143e08b8a9a5431572b195
Ben Walby <ben.walby@googlemail.com>
2021-03-23 22:18:52 +0000

    First non-linear test added.

--- Tests/test_Runner.gd
+++ Tests/test_Runner.gd
@@ -2,13 +2,6 @@ extends "res://addons/gut/test.gd"
 
 const gate_predicate_func: String = "predicate"
 
-var nonlinear_hierarchy_data = [
-	[false, false, false, false],
-	[true, false, true, false],
-	[false, true, true, true],
-	[true, true, true, true]
-]
-
 var one_level_linear_data = [
 	[true],
 	[false]
@@ -43,10 +36,43 @@ func test_2level_linear_test(params=use_parameters(two_level_linear_data)):
 	
 	var input_a = FixedInputComponent.new(input_a_value)
 	var input_b = FixedInputComponent.new(input_b_value)
-	var or_step = OrGate.new().predicate([input_a, input_b])
+	var input_steps = [input_a, input_b]
+	var or_component = ComponentHelper.create_or_component(input_steps)
 	
-	var result = Runner.new().run(or_step)
+	var result = Runner.new().run(or_component)
 	assert_typeof(result, TYPE_ARRAY)
-	assert_all_steps_processed([input_a, input_b, or_step])
+	assert_all_steps_processed([input_a, input_b, or_component])
 	assert_eq(result.size(), 1)
 	assert_eq(result[0], expected_result)
+
+# TODO: nonlinear a,b -> and, and,c -> final
+
+var three_level_non_linear_data = [
+	[false, false, false],
+	[true, false, false],
+	[false, true, true],
+	[true, true, true]
+]
+
+func three_level_non_linear_test(params=use_parameters(three_level_non_linear_data)):
+	var input_a_value = params[0]
+	var input_b_value = params[1]
+	var expected_result = params[2]
+
+	var input_a = FixedInputComponent.new(input_a_value)
+	var input_b = FixedInputComponent.new(input_b_value)
+	var input_steps = [input_a, input_b]
+	var or_component = ComponentHelper.create_or_component(input_steps)
+	var and_component = ComponentHelper.create_and_component([or_component, input_b])
+	
+	var result = Runner.new().run(and_component)
+	assert_typeof(result, TYPE_ARRAY)
+	assert_all_steps_processed([input_a, input_b, or_component, and_component])
+	assert_eq(result.size(), 1)
+	assert_eq(result[0], expected_result)
+
+# TODO: WorkerNonLinear3LevelMultipleOutputTest
+# TODO: WorkerNandOutputTest
+# TODO: WorkerConstructedOrIcOutputTest
+# TODO: WorkerNorOutputTest
+# TODO: WorkerXorOutputTest
