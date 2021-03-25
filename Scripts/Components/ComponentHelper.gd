f0b1b3772fd7053853c737d138ebf465618eee7b
Ben Walby <ben.walby@googlemail.com>
2021-03-25 21:42:53 +0000

    Not, nand convenience functions added to helper

--- Scripts/Components/ComponentHelper.gd
+++ Scripts/Components/ComponentHelper.gd
@@ -10,3 +10,11 @@ static func create_or_component(inputs: Array) -> Component:
 static func create_and_component(inputs: Array) -> Component:
 	var and_gate = AndGate.new()
 	return Component.new(inputs, funcref(and_gate, gate_predicate_func))
+
+static func create_not_component(inputs: Array) -> Component:
+	var or_gate = NotGate.new()
+	return Component.new(inputs, funcref(or_gate, gate_predicate_func))
+
+static func create_nand_component(inputs: Array) -> Component:
+	var and_component = create_and_component(inputs)
+	return create_not_component([and_component])
