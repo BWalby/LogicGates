e66effdfa1bb4c6a76264a403eb19974526aba72
Ben Walby <ben.walby@googlemail.com>
2021-04-22 20:39:35 +0100

    Converted GraphBuilder tests, failing to be fixed.

--- Tests/test_GraphBuilder.gd
+++ Tests/test_GraphBuilder.gd
@@ -22,7 +22,7 @@ func test_ordered_graph_2_inputs_to_combinator():
   var graph = GraphBuilder.get_ordered_graph(final_combinator)
   assert_array_size(graph, 3)
 	
-	#todo: investigate - why is this the wrong way round in the results and not as expected below, a=0, b=1
+	# TODO: investigate - why is this the wrong way round in the results and not as expected below, a=0, b=1
   assert_component_at_index(graph, input_a, 0)
   assert_component_at_index(graph, input_b, 1)
   assert_component_at_index(graph, final_combinator, 2)
@@ -53,162 +53,158 @@ func test_ordered_graph_2_inputs_2_ands_then_combinator():
 # B--/		|
 #			Combinator
 # C--NOT--/  
-# func test_ordered_graph_top_weighted_branch():
+func test_ordered_graph_top_weighted_branch():
+  var input_a = factory.create_input_component(true, "InputA")
+  var input_b = factory.create_input_component(true, "InputB")
+  var inputs_a_b = [input_a, input_b]
+  var and_component = factory.create_and_component(inputs_a_b, "And")
+	
+  var input_c = factory.create_input_component(true, "InputC")
+  var not_component = factory.create_not_component(input_c, "Not")
+
+  var final_combinator = factory.create_pass_through_component([and_component, not_component], "FinalCombinator")
+
+  var graph = GraphBuilder.get_ordered_graph(final_combinator)
+  assert_array_size(graph, 6)
+
+  # TODO: which way round is correct for inputs (added retrospectively)
+  # either this (top_weighted) or bottom_weighted
+  assert_component_at_index(graph, input_a, 0)
+  assert_component_at_index(graph, input_b, 1)
+  assert_component_at_index(graph, input_c, 2)
+  assert_component_at_index(graph, not_component, 3)
+  assert_component_at_index(graph, and_component, 4)
+  assert_component_at_index(graph, final_combinator, 5)
+
+# A--NOT--\
+#			Combinator
+# B--\		|
+#	  AND--/
+# C--/
+func test_ordered_graph_bottom_weighted_branch():
+  var input_a = factory.create_input_component(true, "InputA")
+  var not_component = factory.create_not_component(input_a, "Not")
+
+  var input_b = factory.create_input_component(true, "InputB")
+  var input_c = factory.create_input_component(true, "InputC")
+  var inputs_b_c = [input_b, input_c]
+  var and_component = factory.create_and_component(inputs_b_c, "And")
+
+  var final_combinator = factory.create_pass_through_component([not_component, and_component], "FinalCombinator")
+
+  var graph = GraphBuilder.get_ordered_graph(final_combinator)
+  assert_array_size(graph, 6)
+
+  assert_component_at_index(graph, input_a, 0)
+  assert_component_at_index(graph, input_b, 1)
+  assert_component_at_index(graph, input_c, 2)
+  assert_component_at_index(graph, not_component, 3)
+  assert_component_at_index(graph, and_component, 4)
+  assert_component_at_index(graph, final_combinator, 5)
+	
+# A---AND---\
+#     |      AND---\
+# B--/------/-------Combinator
+func test_ordered_graph_2_ands_a_passed_to_second_then_combinator():
+  var input_a = factory.create_input_component(true, "InputA")
+  var input_b = factory.create_input_component(true, "InputB")
+  var first_and = factory.create_and_component([input_a, input_b], "And1")
+  var second_and = factory.create_and_component([first_and, input_b], "And2")
+  var final_combinator = factory.create_pass_through_component([second_and, input_b], "FinalCombinator")
+
+  var graph = GraphBuilder.get_ordered_graph(final_combinator)
+  assert_array_size(graph, 5)
+
+  assert_component_at_index(graph, input_a, 0)
+  assert_component_at_index(graph, input_b, 1)
+
+  assert_component_at_index(graph, first_and, 2)
+  assert_component_at_index(graph, second_and, 3)
+
+  assert_component_at_index(graph, final_combinator, 4)
+
+# A--\------\
+#     |     AND-----Combinator
+#     AND--/        |
+# B--/--------------/
+func test_ordered_graph_2_ands_a_passed_to_second_b_passed_combinator():
+  var input_a = factory.create_input_component(true, "InputA")
+  var input_b = factory.create_input_component(true, "InputB")
+  var first_and = factory.create_and_component([input_a, input_b], "And1")
+  var second_and = factory.create_and_component([input_a, first_and], "And2")
+  var final_combinator = factory.create_pass_through_component([second_and, input_b], "FinalCombinator")
+
+  var graph = GraphBuilder.get_ordered_graph(final_combinator)
+  assert_array_size(graph, 5)
+
+  assert_component_at_index(graph, input_a, 0)
+  assert_component_at_index(graph, input_b, 1)
+
+  assert_component_at_index(graph, first_and, 2)
+  assert_component_at_index(graph, second_and, 3)
+
+  assert_component_at_index(graph, final_combinator, 4)
+
+# A-PasThrough-\
+#               AND---PasThrough
+# B-PasThrough-/      |
+# C-PasThrough--------/  
+func test_ordered_graph_2_buffered_inputs_to_and_1_buffered_input_to_combinator_with_and():
+  var input_a = factory.create_input_component(true, "InputA")
+  var input_b = factory.create_input_component(true, "InputB")
+  var input_c = factory.create_input_component(true, "InputC")
+  var buffer_a = factory.create_pass_through_component([input_a], "BufferA")
+  var buffer_b = factory.create_pass_through_component([input_b], "BufferB")
+  var buffer_c = factory.create_pass_through_component([input_c], "BufferC")
+  var and_component = factory.create_and_component([buffer_a, buffer_b], "And")
+  var final_combinator = factory.create_pass_through_component([and_component, buffer_c], "FinalCombinator")
+
+  var graph = GraphBuilder.get_ordered_graph(final_combinator)
+  assert_array_size(graph, 8)
+
+  var input_a_index = graph.find(input_a)
+  var input_b_index = graph.find(input_b)
+  var input_c_index = graph.find(input_c)
+  var buffer_a_index = graph.find(buffer_a)
+  var buffer_b_index = graph.find(buffer_b)
+  var buffer_c_index = graph.find(buffer_c)
+  var and_index = graph.find(and_component)
+  var combinator_index = graph.find(final_combinator)
+
+  assert_eq(input_c_index, 0)
+  assert_eq(input_b_index, 1)
+  assert_eq(input_a_index, 2)
+
+  assert_eq(buffer_a_index, 3)
+  assert_eq(buffer_b_index, 4)
+  assert_eq(and_index, 5)
+
+  assert_eq(buffer_c_index, 6)
+  assert_eq(combinator_index, 7)
+
+# A-----AND--NOT--\
+#     |			      AND
+# B-----OR--------/
+# func test_ordered_graph_xor_ic():
 #   var input_a = factory.create_input_component(true, "InputA")
 #   var input_b = factory.create_input_component(true, "InputB")
 #   var input_components = [input_a, input_b]
-#   var and_component = factory.create_and_component(input_components, "And")
-	
-#   var input_c = factory.create_input_component(true, "InputC")
-#   var not_component = factory.create_not_component(input_c, "Not")
 
-#   var final_combinator = factory.create_pass_through_component([and_component, and_component], "FinalCombinator")
+#   var and_component = factory.create_and_component(input_components, "And1")
+#   var not_component = factory.create_not_component(and_component, "Not")
 
-#   var graph = GraphBuilder.get_ordered_graph(final_combinator)
+#   # TODO: can't create OR gates just yet, so we can't convert this test right now
+#   var or_component = ComponentHelper.create_or_component(input_components, "or")
+
+#   # NAND and OR, converge onto a single AND
+#   var final_and_component = factory.create_and_component([not_component, or_component], "And2")
+
+#   var graph = GraphBuilder.get_ordered_graph(final_and_component)
 #   assert_array_size(graph, 6)
 
-#   # TODO: which way round is correct for inputs (added retrospectively)
-#   # either this (top_weighted) or bottom_weighted
 #   assert_component_at_index(graph, input_a, 0)
 #   assert_component_at_index(graph, input_b, 1)
-#   assert_component_at_index(graph, input_c, 2)
+#   assert_component_at_index(graph, and_component, 2)
 #   assert_component_at_index(graph, not_component, 3)
-#   assert_component_at_index(graph, and_component, 4)
-#   assert_component_at_index(graph, final_combinator, 5)
-
-# func test_ordered_graph_bottom_weighted_branch():
-# 	# A--NOT--\
-# 	#			Combinator
-# 	# B--\		|
-# 	#	  AND--/
-# 	# C--/
-	
-# 	var input_a = FixedInputComponent.new(true, "a")
-# 	var not_component = ComponentHelper.create_not_component([input_a], "not")
-	
-# 	var input_b = FixedInputComponent.new(true, "b")
-# 	var input_c = FixedInputComponent.new(true, "c")
-# 	var and_component = ComponentHelper.create_and_component([input_b, input_c], "and")
-	
-# 	var final_combinator = CombinatorComponent.new([not_component, and_component], "combinator")
-	
-# 	var graph = GraphBuilder.get_ordered_graph(final_combinator)
-# 	assert_array_size(graph, 6)
-	
-# 	assert_component_at_index(graph, input_a, 0)
-# 	assert_component_at_index(graph, input_b, 1)
-# 	assert_component_at_index(graph, input_c, 2)
-# 	assert_component_at_index(graph, not_component, 3)
-# 	assert_component_at_index(graph, and_component, 4)
-# 	assert_component_at_index(graph, final_combinator, 5)
-	
-	
-# func test_ordered_graph_2_ands_a_passed_to_second_then_combinator():
-# 	# A---AND---\
-# 	#     |      AND---\
-# 	# B--/------/-------Combinator
-
-# 	var input_a = FixedInputComponent.new(true, "a")
-# 	var input_b = FixedInputComponent.new(true, "b")
-# 	var first_and = ComponentHelper.create_and_component([input_a, input_b], "and1")
-# 	var second_and = ComponentHelper.create_and_component([first_and, input_b], "and2")
-# 	var final_combinator = CombinatorComponent.new([second_and, input_b], "combinator")
-
-# 	var graph = GraphBuilder.get_ordered_graph(final_combinator)
-# 	assert_array_size(graph, 5)
-
-# 	assert_component_at_index(graph, input_a, 0)
-# 	assert_component_at_index(graph, input_b, 1)
-
-# 	assert_component_at_index(graph, first_and, 2)
-# 	assert_component_at_index(graph, second_and, 3)
-	
-# 	assert_component_at_index(graph, final_combinator, 4)
-
-# func test_ordered_graph_2_ands_a_passed_to_second_b_passed_combinator():
-# 	# A--\------\
-# 	#     |     AND-----Combinator
-# 	#     AND--/        |
-# 	# B--/--------------/
-
-# 	var input_a = FixedInputComponent.new(true, "a")
-# 	var input_b = FixedInputComponent.new(true, "b")
-# 	var first_and = ComponentHelper.create_and_component([input_a, input_b], "and1")
-# 	var second_and = ComponentHelper.create_and_component([first_and, input_a], "and2")
-# 	var final_combinator = CombinatorComponent.new([second_and, input_b], "combinator")
-
-# 	var graph = GraphBuilder.get_ordered_graph(final_combinator)
-# 	assert_array_size(graph, 5)
-
-# 	assert_component_at_index(graph, input_a, 0)
-# 	assert_component_at_index(graph, input_b, 1)
-
-# 	assert_component_at_index(graph, first_and, 2)
-# 	assert_component_at_index(graph, second_and, 3)
-
-# 	assert_component_at_index(graph, final_combinator, 4)
-
-# func test_ordered_graph_2_buffered_inputs_to_and_1_buffered_input_to_combinator_with_and():
-# 	# A-Combinator-\
-# 	#               AND---Combinator
-# 	# B-Combinator-/      |
-# 	# C-Combinator--------/
-
-# 	var input_a = FixedInputComponent.new(true, "a")
-# 	var input_b = FixedInputComponent.new(true, "b")
-# 	var input_c = FixedInputComponent.new(true, "c")
-# 	var buffer_a = CombinatorComponent.new([input_a], "bufferA")
-# 	var buffer_b = CombinatorComponent.new([input_b], "bufferB")
-# 	var buffer_c = CombinatorComponent.new([input_c], "bufferC")
-# 	var and_component = ComponentHelper.create_and_component([buffer_a, buffer_b], "and1")
-# 	var final_combinator = CombinatorComponent.new([and_component, buffer_c], "combinator")
-
-# 	var graph = GraphBuilder.get_ordered_graph(final_combinator)
-# #	assert_array_size(graph, 8)
-# #
-# #	var input_a_index = graph.find(input_a)
-# #	var input_b_index = graph.find(input_b)
-# #	var input_c_index = graph.find(input_c)
-# #	var buffer_a_index = graph.find(buffer_a)
-# #	var buffer_b_index = graph.find(buffer_b)
-# #	var buffer_c_index = graph.find(buffer_c)
-# #	var and_index = graph.find(and_component)
-# #	var combinator_index = graph.find(final_combinator)
-# #
-# #	assert_eq(input_c_index, 0)
-# #	assert_eq(input_a_index, 1)
-# #	assert_eq(input_a_index, 2)
-# #
-# #	assert_eq(buffer_a_index, 3)
-# #	assert_eq(buffer_b_index, 4)
-# #	assert_eq(and_index, 5)
-# #
-# #	assert_eq(buffer_c_index, 6)
-# #	assert_eq(combinator_index, 7)
-
-# func test_ordered_graph_xor_ic():
-# 	# A-----AND--NOT--\
-# 	#    |			  AND
-# 	# B-----OR--------/
-	
-# 	var input_a = FixedInputComponent.new(true, "a")
-# 	var input_b = FixedInputComponent.new(true, "b")
-# 	var input_components = [input_a, input_b]
-	
-# 	var and_component = ComponentHelper.create_and_component(input_components, "and1")
-# 	var not_component = ComponentHelper.create_not_component([and_component], "not")
-	
-# 	var or_component = ComponentHelper.create_or_component(input_components, "or")
-
-# 	# NAND and OR, converge onto a single AND
-# 	var final_and_component = ComponentHelper.create_and_component([not_component, or_component], "and2")
-	
-# 	var graph = GraphBuilder.get_ordered_graph(final_and_component)
-# 	assert_array_size(graph, 6)
-
-# 	assert_component_at_index(graph, input_a, 0)
-# 	assert_component_at_index(graph, input_b, 1)
-# 	assert_component_at_index(graph, and_component, 2)
-# 	assert_component_at_index(graph, not_component, 3)
-# 	assert_component_at_index(graph, or_component, 4)
-# 	assert_component_at_index(graph, final_and_component, 5)
+#   assert_component_at_index(graph, or_component, 4)
+#   assert_component_at_index(graph, final_and_component, 5)
