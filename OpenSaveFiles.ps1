2da37f76e90116d16891b33e2e011f6f6c3c953b
Ben Walby <ben.walby@googlemail.com>
2021-07-29 08:41:29 +0100

    Script to open data files in vs code.

---
+++ OpenSaveFiles.ps1
@@ -0,0 +1,5 @@
+$originalLocation = Get-Location
+Set-Location 'C:\Users\benwa\AppData\Roaming\Godot\app_userdata\Logic Gates'
+code.cmd '.\components.save'
+code.cmd '.\defs.save'
+Set-Location $originalLocation
